f7821e6d215decba2a3756d719dc1c18d1250179
brendanGiraudet <brendan.giraudet1@gmail.com>
2024-09-19 09:31:32 +0200

    #é - add validation into recip form

--- application-recip/Components/Pages/RecipForm/RecipForm.razor.cs
+++ application-recip/Components/Pages/RecipForm/RecipForm.razor.cs
@@ -1,18 +1,28 @@
-﻿using ms_recip.Ms_recip.Models;
+﻿using application_recip.Stores.BaseStore.Actions;
+using application_recip.Stores.RecipsStore;
+using Fluxor;
+using Microsoft.AspNetCore.Components;
+using ms_recip.Ms_recip.Models;
 
 namespace application_recip.Components.Pages.RecipForm;
 
 public partial class RecipForm
 {
+    [Inject] public required IState<RecipsState> RecipsState { get; set; }
+
+    [Inject] public required IDispatcher Dispatcher { get; set; }
+
+    [Inject] public required NavigationManager NavigationManager { get; set; }
+
     RecipModel _actualRecip = new RecipModel();
 
     void Submit(RecipModel model)
     {
-        //
+        Dispatcher.Dispatch(new CreateItemAction<RecipModel>(model));
     }
 
     void Cancel()
     {
-        //
+        // redirect to list
     }
 }
